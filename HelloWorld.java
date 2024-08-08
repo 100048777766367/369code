ceab418abd064fafadc62b449b0e25946e0374fe
100048777766367 <112754045+100048777766367@users.noreply.github.com>
2024-08-08 11:18:53 +0700

    comm

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main (String[ args]){
+        int a=1;
+        //In roi moi dang
+        System.out.print(a++);
+        // Tang roi moi in
+        System.out.println(++a);
+        //Cong 1 va in ra roi moi tang
+        System.out.println(a+++1);
+        //tang roi moi cong 1 va in ra
+        System.out.println(++a+1);
+        
+    }
+}
